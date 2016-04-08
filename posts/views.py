583ffd7beae2e009ff69eb964c17526e747d8354
Den4i <dglonassik@gmail.com>
2016-04-08 15:30:50 +0300

    Добавление в настойки и url проекта папки MEDIA_ROOT Добавление пагинации в представление post_list.

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponseRedirect
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 
-from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from .forms import PostForm
 
@@ -47,12 +48,24 @@ def post_detail(request, id=None):
 
 
 def post_list(request):
-    queryset = Post.objects.all()
+    queryset_list = Post.objects.all()
+    paginator = Paginator(queryset_list, 10)    # Show 10 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
+
     context = {
         'object_list': queryset,
         "title": "list"
     }
-    return render(request, "posts/index.html", context)
+    return render(request, "posts/post_list.html", context)
 
 
 def post_delete(request, id=None):
@@ -60,3 +73,5 @@ def post_delete(request, id=None):
     instance.delete()
     messages.success(request, "Successfully deleted")
     return redirect("posts:list")
+
+
