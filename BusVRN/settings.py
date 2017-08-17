c021c39157a9e050c4b0932c2ecfa837bc3ae50a
Den4i <dglonassik@gmail.com>
2017-08-17 23:56:29 +0300

    ref

--- BusVRN/settings.py
+++ BusVRN/settings.py
@@ -147,8 +147,8 @@ STATICFILES_DIRS = [
 ]
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = "staticfiles"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
