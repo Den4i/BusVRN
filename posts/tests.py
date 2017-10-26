456d806c6da73a1740c2ea113458d4274966dad9
Den4i <dglonassik@gmail.com>
2017-10-26 23:40:49 +0300

    Add test for posts

--- posts/tests.py
+++ posts/tests.py
@@ -1,16 +1,22 @@
 from django.test import TestCase
-
-# Create your tests here.
+from django.contrib.auth.models import User
 
 from posts.models import Post
-from django.contrib.auth.models import User
 
 
 class PostMethodTests(TestCase):
-    user = User.objects.get(username='john2')
+    user = User.objects.get(username='wombat')
 
     def test_content_not_null(self):
-        post = Post(user=self.user, title='', slug='', content='', publish='2016-08-22', read_time=1, )
+        post = Post(user=self.user, title='test_title', slug='', content='test_content', publish='2016-08-22', read_time=1,)
         post.save()
         self.assertEqual((len(post.title) > 0), True)
         self.assertEqual((len(post.content) > 0), True)
+
+    def test_setUp(self):
+        self.posts = []
+        for i in range(20):
+            post = Post(user=self.user, title='test_title%d' % i, slug='', content='test_content', publish='2016-08-22', read_time=1,)
+            self.posts.append(post)
+
+        self.assertEqual(len(self.posts), 20)
