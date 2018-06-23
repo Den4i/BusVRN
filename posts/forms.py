daabd722bfbc825124ce6451c434c9880ef43f93
Den4i <dglonassik@gmail.com>
2018-06-23 11:26:50 +0300

    Changr draft field

--- posts/forms.py
+++ posts/forms.py
@@ -7,7 +7,7 @@ class PostForm(forms.ModelForm):
     content = forms.CharField(widget=PagedownWidget(show_preview=False), label='Содержание')
     publish = forms.DateField(widget=forms.SelectDateWidget, label='Дата публикации')
     image = forms.FileField(widget=forms.FileInput, label='Изображение')
-    draft = forms.ChoiceField(widget=forms.CheckboxInput, label='Черновик')
+    draft = forms.BooleanField(widget=forms.CheckboxInput, label='Черновик')
 
     class Meta:
         model = Post
