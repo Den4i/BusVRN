724ec9352221196739ebda6979d362e48063c043
Den4i <dglonassik@gmail.com>
2018-06-23 11:28:31 +0300

    Change static files

--- static/js/post/post-ajax.js
+++ static/js/post/post-ajax.js
@@ -1,8 +1,6 @@
 $('#likes').click(function(){
-    var postid;
-    postid = $(this).attr("data-postid");
+    let postid = $(this).attr("data-postid");
     $.get('/like_post/', {post_id: postid}, function(data){
-               $('#like_count').html(data);
-               $('#likes').hide();
+        $('#like_count').html(data);
     });
 });
