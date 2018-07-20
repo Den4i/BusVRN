3f39eb16a9d45c1e9a6fd5b3bfbc1a36f7d8d18a
Den4i <dglonassik@gmail.com>
2018-07-20 23:03:00 +0300

    change urls on version django2

--- polls/urls.py
+++ polls/urls.py
@@ -1,15 +1,12 @@
-from django.urls import re_path
+from django.urls import path
 
 from . import views
 
 app_name = 'polls'
 
 urlpatterns = [
-    re_path(r'^$', views.IndexView.as_view(), name='index'),
-    # ex: /polls/5/
-    re_path(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    # ex: /polls/5/results/
-    re_path(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
-    # ex: /polls/5/vote/
-    re_path(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
