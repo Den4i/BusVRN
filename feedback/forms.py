1b7c6f6d4563e33f4866f30884aa5c5c243018fc
Денис Папилин <d.papilin@litebox.ru>
2019-10-29 14:32:09 +0400

    Использование django-phonenumber-field для работы с телефонными полями

--- feedback/forms.py
+++ feedback/forms.py
@@ -1,11 +1,12 @@
 from django import forms
+from phonenumber_field.formfields import PhoneNumberField
 from .models import Feedback
 
 
 class FeedbackForm(forms.ModelForm):
     subject = forms.CharField(label='Тема обращения')
     feedtext = forms.CharField(widget=forms.Textarea(attrs={'rows': 10, 'cols': 10}), label='Содержание')
-    phone = forms.CharField(label='Контактный телефон', max_length=11)
+    phone = PhoneNumberField(label='Контактный телефон')
 
     class Meta:
         model = Feedback
--- feedback/models.py
+++ feedback/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from django.conf import settings
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Feedback(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
-    subject = models.TextField()
-    feedtext = models.TextField()
-    phone = models.BigIntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE, verbose_name='Пользователь')
+    subject = models.TextField(verbose_name='Тема')
+    feedtext = models.TextField(verbose_name='Текст')
+    phone = PhoneNumberField(verbose_name='Телефон')
+
+    class Meta:
+        verbose_name_plural = 'Обратная связь'
+
+    def __str__(self):
+        return "%s/%s" % (self.user, self.subject)
