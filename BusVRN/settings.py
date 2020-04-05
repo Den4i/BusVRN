c27d8de96ee18ef020ab48c803591f792550b990
Денис Папилин <d.papilin@litebox.ru>
2020-04-05 17:07:00 +0400

    Обновление статики, смена бд

--- BusVRN/settings.py
+++ BusVRN/settings.py
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'BusVRN.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'busvrn',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
