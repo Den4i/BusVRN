65e3393eb01ce282f4cddd85ce69960bbdcedc73
Денис Папилин <d.papilin@litebox.ru>
2020-04-12 21:24:53 +0400

    В форме поста дата публикации по-умолчанию сегодняшняя

--- posts/forms.py
+++ posts/forms.py
@@ -1,11 +1,12 @@
 from django import forms
 from pagedown.widgets import PagedownWidget
+import datetime
 from .models import Post
 
 
 class PostForm(forms.ModelForm):
     content = forms.CharField(widget=PagedownWidget(show_preview=False), label='Содержание')
-    publish = forms.DateField(widget=forms.SelectDateWidget, label='Дата публикации')
+    publish = forms.DateField(widget=forms.SelectDateWidget, label='Дата публикации', initial=datetime.date.today)
     image = forms.FileField(widget=forms.FileInput, label='Изображение')
     draft = forms.BooleanField(widget=forms.CheckboxInput, label='Черновик', required=False)
 
