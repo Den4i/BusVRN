ca07dabf50c1be31e442b459ebbc669034e3c740
Денис Папилин <d.papilin@litebox.ru>
2020-07-19 16:32:21 +0400

    Update libraries

--- posts/forms.py
+++ posts/forms.py
@@ -5,7 +5,7 @@ from .models import Post
 
 
 class PostForm(forms.ModelForm):
-    content = forms.CharField(widget=PagedownWidget(show_preview=False), label='Содержание')
+    content = forms.CharField(widget=PagedownWidget(attrs={"show_preview":False}), label='Содержание')
     publish = forms.DateField(widget=forms.SelectDateWidget, label='Дата публикации', initial=datetime.date.today)
     image = forms.FileField(widget=forms.FileInput, label='Изображение')
     draft = forms.BooleanField(widget=forms.CheckboxInput, label='Черновик', required=False)
